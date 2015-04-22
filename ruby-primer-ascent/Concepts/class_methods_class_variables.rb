17dd9718db01f56a3ed1b91e14e77ccc28a231a7
Tien Ta Minh <taminhtien1993@gmail.com>
2015-04-22 10:39:29 +0700

    Add Class variables and inheritance concept and examples

--- ruby-primer-ascent/Concepts/class_methods_class_variables.rb
+++ ruby-primer-ascent/Concepts/class_methods_class_variables.rb
@@ -82,3 +82,98 @@ ApplicationConfiguration.set("version", "0.1")
 
 p ApplicationConfiguration.get("version")
 p ApplicationConfiguration.get("name")
+
+# Class variables and inheritance
+
+class ApplicationConfiguration
+  @@configuration = {}
+
+  def self.set(property, value)
+    @@configuration[property] = value
+  end
+
+  def self.get(property)
+    @@configuration[property]
+  end
+end
+
+class ERPApplicationConfiguration < ApplicationConfiguration
+end
+
+class WebApplicationConfiguration < ApplicationConfiguration
+end
+
+ERPApplicationConfiguration.set("name", "ERP Application")
+WebApplicationConfiguration.set("name", "Web Application")
+
+p ERPApplicationConfiguration.get("name")
+p WebApplicationConfiguration.get("name")
+
+p ApplicationConfiguration.get("name") # ==> any changes in any class will affect to other class
+# They all share the same copy of the class variable
+
+# Class instance variable
+
+class Foo
+  @foo_count = 0 # ==> class instance variable != instance variable
+  # @foo is initialized directly in the class body and is accessed only from class methods.
+
+  def self.increment_counter
+    @foo_count += 1
+  end
+
+  def self.current_count
+    @foo_count
+  end
+end
+
+class Bar < Foo
+  @foo_count = 100
+end
+
+Foo.increment_counter
+Bar.increment_counter
+p Foo.current_count
+p Bar.current_count
+
+# Ex:
+
+class ApplicationConfiguration
+  @configuration = {} # ==> only one @
+
+  def self.set(property, value)
+    @configuration[property] = value
+  end
+
+  def self.get(property)
+    @configuration[property]
+  end
+end
+
+class ERPApplicationConfiguration < ApplicationConfiguration
+  @configuration = {}
+end
+
+class WebApplicationConfiguration < ApplicationConfiguration
+  @configuration = {}
+end
+
+ERPApplicationConfiguration.set("name", "ERP Application")
+WebApplicationConfiguration.set("name", "Web Application")
+
+p ERPApplicationConfiguration.get("name")
+p WebApplicationConfiguration.get("name")
+
+p ApplicationConfiguration.get("name")
+
+# Class instance variables are a better alternative than class variables
+# simply because the data is not shared across the inheritance chain.
+# Another major difference between class variables and class instance variables
+# are that class instance variables are available only in class methods.
+# But class variables are available in both class methods and instance methods.
+
+# Instance variables are available only for instances of a class.
+# They look like @name. Class variables are available to both class methods and instance methods.
+# They look like @@name
+                                                                                                                            It is almost always a bad idea to use a class variable to store state. There are only a very few valid use cases where class variables are the right choice.
+                                                                                                                                                                                                                                                                                     Prefer class instance variables over class variables when you do really need store data at a class level. Class instance variables use the same notation as that of an instance variable. But unlike instance variables, you declare them inside the class definition directly.
